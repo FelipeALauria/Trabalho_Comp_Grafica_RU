446ab5d23dc9fdd9ff308287179837006ac13c5c
FelipeALauria <fel.lauria@gmail.com>
2025-10-26 02:02:20 -0300

    Primeiro Commit

---
+++ ru.c
@@ -0,0 +1,13 @@
+/* RU, praça de esporte e negócio dos velhos
+
+    Pra compilar gcc <fonte.c> -o <fonte> -lglut -lGL -lGLU –lm
+*/
+
+#include <stdio.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, RU!\n");
+    return 0;
+}
