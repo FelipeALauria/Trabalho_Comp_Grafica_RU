5568af537254438d2589e70dc81dd09bbc6cfa68
FelipeALauria <fel.lauria@gmail.com>
2025-10-26 02:22:01 -0300

    Segundo Commit

--- ru.c
+++ ru.c
@@ -7,7 +7,27 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Hello, RU!\n");
+void display();
+
+void display(){
+    glClearColor(0.4 , 0.6, 0, 0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-200, 200, -200, 200);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("RU");
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
